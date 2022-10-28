4680bae79d96dd10f1eabd212a905a8b6edc4c26
neemaHbim <habimananeema@gmail.com>
2022-10-28 11:05:46 +0200

    first commit

---
+++ src/Pages/AddProduct.js
@@ -0,0 +1,26 @@
+import Header from "../components/Header";
+
+function AddProduct(){
+return(
+<div>
+<Header/>
+
+<div className="col-sm-6 offset-sm-3">
+<br/>
+<input type="type" className="form-control" />
+<input/>
+
+
+</div>
+
+
+</div>
+
+
+)
+
+
+
+}
+
+export default A
